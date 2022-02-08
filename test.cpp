e66371b0f715fce66aaa71ee66e1ee6162bb9830
Andrew Higgins <anhi5992@colorado.edu>
2022-02-08 16:13:16 -0700

    Adding test file

--- test.cpp
+++ test.cpp
@@ -3,4 +3,8 @@
 #include "functions_to_implement.cpp"
 
 
-TEST_CASE("Number of 2s computed", [")
+TEST_CASE("Number of 2s computed", "[new]")
+{REQUIRE( RemoveTwos(16) == 1);
+}
+TEST_CASE("Figure the sum of the vector values", "[new]")
+{REQUIRE(Sum(1)}
